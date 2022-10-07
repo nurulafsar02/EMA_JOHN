f1b2a1d41630e185fb87ce7f95cdc91c71b13da7
nurulafsar02 <khiladimoshin@gmail.com>
2022-10-08 00:42:01 +0600

    cart add to db

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+
+function App() {
+  return (
+    <div className='display-container'>
+      <Header></Header>
+      <Shop></Shop>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <div>
+            <nav className='navigation'>
+                <img src={logo} alt=""/>
+                <div> 
+                <a href="/shop">Shop</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/odear">Odear</a>
+                <a href="/about">About</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
